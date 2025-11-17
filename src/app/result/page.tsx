88714516ab531f7850146d2e122ea1a5d75ce5c2
Rebowa Miftahurrahman <miftahrebowa@gmail.com>
2025-11-17 22:50:38 +0700

    update: result page and add separated result-page file

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,54 +1,10 @@
-"use client";
+import { Suspense } from "react";
+import ResultPage from "./result-page";
 
-import { useSearchParams } from "next/navigation";
-import { 
-  getAllDivisionsOfStudent, 
-  getGroupLinkByDivision, 
-  divisionGroups 
-} from "@/lib/division";
-import RevealCard from "../components/revealcard";
-import ButtonJoinGroup from "../components/button";
-
-export default function ResultPage() {
-  const searchParams = useSearchParams();
-  const name = searchParams.get("pageName")?.trim() || "";
-  const kelas = searchParams.get("kelas")?.trim() || "";
-
-  // ambil semua divisi siswa
-  const studentDivisions = getAllDivisionsOfStudent(name, kelas);
-
-  // jika siswa tidak ada sama sekali
-  if (studentDivisions.length === 0) {
-    return (
-      <p className="text-center mt-20 text-red-500 text-lg font-semibold">
-        Siswa tidak ditemukan 😢
-      </p>
-    );
-  }
-
+export default function Page() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-cyan-50 p-4 gap-6">
-
-      {studentDivisions.map((student, index) => {
-        // info divisi
-        const divInfo = divisionGroups.find(d => d.division === student.division);
-        const divisionName = divInfo ? divInfo.name : `Divisi ${student.division}`;
-
-        // link grup tiap divisi
-        const groupLink = getGroupLinkByDivision(student.division);
-
-        return (
-          <div key={index} className="flex flex-col items-center gap-3">
-            <RevealCard 
-              student={student}
-              divisionName={divisionName}
-            />
-
-            <ButtonJoinGroup link={groupLink} />
-          </div>
-        );
-      })}
-
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <ResultPage />
+    </Suspense>
   );
 }
